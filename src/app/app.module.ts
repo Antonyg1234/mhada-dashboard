4502ae94238a9818fd5910b33d76a9c755fac595
antony.george <antony.george@neosofttech.com>
2019-09-16 11:58:37 +0530

    first commit of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentAreaComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
