6a514ae9670147ead6f08f0ec0e5f6305b791070
Anway Bhagwat <anwaybhg@gmail.com>
2019-10-04 18:25:38 +0530

    project listing

--- src/app/content-area/views/project-detail/project-detail.component.ts
+++ src/app/content-area/views/project-detail/project-detail.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-
+import { ProjectDetail, ProjectDetailService } from './project-detail.service';
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
-  styleUrls: ['./project-detail.component.css']
+  styleUrls: ['./project-detail.component.css'],
+  providers:[ProjectDetailService]
 })
 export class ProjectDetailComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute) { }
+  projects:ProjectDetail[];
+  constructor(private route: ActivatedRoute,private service:ProjectDetailService) { }
 
   ngOnInit() {
-    let param1 = this.route.snapshot.queryParams["board_id"];
+    let param1 = this.route.snapshot.paramMap.get("board_id");
     console.log(param1);
+    this.getList(param1);
+  }
+
+  getList(board_id): void {
+    this.service.getList(board_id)
+      .subscribe(projects => (this.projects = projects['data']));
   }
 
 }
