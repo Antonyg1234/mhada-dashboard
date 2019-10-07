43d43f2e82c0689c7e623b4140cf1379de4db184
antony.george <antony.george@neosofttech.com>
2019-10-07 14:19:35 +0530

    breadcrumb handled

--- src/app/content-area/breadcrumb/breadcrumb.component.ts
+++ src/app/content-area/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersistanceService } from './../../services/persistanceService.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./breadcrumb.component.css']
 })
 export class BreadcrumbComponent implements OnInit {
+  public href: string = "";
+  public dash_name: string = "";
+  public board_id: number;
 
-  constructor() { }
+  public page_type:string = "";
+  constructor(private router: Router,private persistanceService:PersistanceService) { }
 
   ngOnInit() {
+    this.href = this.router.url;
+
+    if(this.href == '/boards'){
+      this.dash_name = 'Boards';
+      this.page_type = 'boards';
+    }else if(this.href.includes('/projects')){
+      this.dash_name = 'Projects';
+      this.page_type = 'projects';
+    }else if(this.href.includes('/dashboard')|| this.href.includes('/modules')){
+      this.dash_name = 'Modules/Dashboard';
+      this.page_type = 'modules';
+      this.board_id = this.persistanceService.get('selectedBoard');
+    }
   }
 
 }
