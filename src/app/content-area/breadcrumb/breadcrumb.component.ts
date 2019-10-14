e455d65bd3ea5a00c126beb16540ef55ed180b4d
sachin <sachin.mhaskar@wwindia.com>
2019-10-14 12:32:42 +0530

    Fixed breadcrumb

--- src/app/content-area/breadcrumb/breadcrumb.component.ts
+++ src/app/content-area/breadcrumb/breadcrumb.component.ts
@@ -7,39 +7,43 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.css']
 })
+
 export class BreadcrumbComponent implements OnInit {
   public href: string = "";
   public dash_name: string = "";
   public board_id: number;
   public selected_board:string;
   public page_type:string = "";
-  selected_board_id: number;
   constructor(private route: ActivatedRoute, private persister: PersistanceService, private router: Router,private persistanceService:PersistanceService) { }
 
   ngOnInit() {
     this.href = this.router.url;
     let that = this;
-	  this.route.paramMap.subscribe(params => {
-      let board_id = params.get("board_id");
-      that.selected_board_id    =     parseInt(board_id);
-      that.selected_board=that.persister.get('boardsData').find(x=>x.id==board_id);
-    })
+    this.route.paramMap.subscribe(params => {
 
-    if(this.href == '/boards'){
-      this.dash_name = 'Boards';
-      this.page_type = 'boards';
-    }else if(this.href.includes('/projects')){
-      this.dash_name = this.selected_board['description'];
-      this.page_type = 'projects';
-    }else if(this.href.includes('/modules')){
-      this.dash_name = this.selected_board['description'];
-      this.page_type = 'modules';
-      this.board_id = this.persistanceService.get('selectedBoard');
-    }else if(this.href.includes('/dashboard')){
-      this.dash_name = this.selected_board['description'];
-      this.page_type = 'dashboard';
-      this.board_id = this.persistanceService.get('selectedBoard');
-    }
+    	if(this.href.includes('/projects')) {
+		    let board_id = params.get("board_id");
+		    this.selected_board = that.persister.get('boardsData').find(x => x.id == board_id);
+	    }else{
+		    let board_id = that.persister.get("selectedBoard");
+		    this.selected_board = that.persister.get('boardsData').find(x => x.id == parseInt(board_id));
+	    }
+
+	    if(this.href == '/boards'){
+		    this.dash_name = 'Boards';
+		    this.page_type = 'boards';
+	    }else if(this.href.includes('/projects')){
+		    this.dash_name = this.selected_board['description'];
+		    this.page_type = 'projects';
+	    }else if(this.href.includes('/modules')){
+		    this.dash_name = this.selected_board['description'];
+		    this.page_type = 'modules';
+		    this.board_id = this.persistanceService.get('selectedBoard');
+	    }else if(this.href.includes('/dashboard')){
+		    this.dash_name = this.selected_board['description'];
+		    this.page_type = 'dashboard';
+		    this.board_id = this.persistanceService.get('selectedBoard');
+	    }
+    });
   }
-
 }
